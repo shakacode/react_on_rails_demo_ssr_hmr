2e79580110bb450d4986e0fca48ed2f1d9b0409f
Justin Gordon <justin.gordon@gmail.com>
2020-08-02 13:37:05 -1000

    rails generate react_on_rails:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
