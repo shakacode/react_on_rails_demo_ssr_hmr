7e53803fce7034f5ecff335db1f400a5743a87e7
Justin Gordon <justin.gordon@gmail.com>
2020-08-02 13:50:12 -1000

    Add HMR and Fast Refresh
    https://github.com/pmmmwh/react-refresh-webpack-plugin
    
    After this change, you can run
    
    foreman start -f Procfile.dev-hmr
    
    and you can edit a JSX file and save and see the React change and the
    component state was preserved.

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,22 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const { devServer } = require('@rails/webpacker')
+
 const environment = require('./environment')
+
+const isWebpackDevServer = process.env.WEBPACK_DEV_SERVER
+
+//plugins
+if (isWebpackDevServer) {
+  environment.plugins.append(
+    'ReactRefreshWebpackPlugin',
+    new ReactRefreshWebpackPlugin({
+      overlay: {
+        sockPort: devServer.port
+      }
+    })
+  )
+}
 
 module.exports = environment.toWebpackConfig()
