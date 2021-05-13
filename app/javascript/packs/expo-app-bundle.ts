e94ef03fe8e61f4c3e78cd62947ba75ff5165375
pulkitkkr <pulkitkkr@gmail.com>
2021-05-14 05:04:33 +0530

    Add React-Native Alias and register ExpoApp component

---
+++ app/javascript/packs/expo-app-bundle.ts
@@ -0,0 +1,8 @@
+import ReactOnRails from 'react-on-rails'
+
+import ExpoApp from '../bundles/expo-app/App'
+
+// This is how react_on_rails can see the HelloWorld in the browser.
+ReactOnRails.register({
+  ExpoApp
+})
--- config/webpack/commonWebpackConfig.js
+++ config/webpack/commonWebpackConfig.js
@@ -1,14 +1,21 @@
 // Common configuration applying to client and server configuration
 
-const { webpackConfig: baseClientWebpackConfig, merge } = require('@rails/webpacker')
+const {
+  webpackConfig: baseClientWebpackConfig,
+  merge
+} = require('@rails/webpacker')
 
 const commonOptions = {
   resolve: {
-      extensions: ['.css', '.ts', '.tsx']
+    extensions: ['.css', '.ts', '.tsx'],
+    alias: {
+      'react-native': 'react-native-web'
+    }
   }
 }
 
 // Copy the object using merge b/c the baseClientWebpackConfig and commonOptions are mutable globals
-const commonWebpackConfig = () => (merge({}, baseClientWebpackConfig, commonOptions))
+const commonWebpackConfig = () =>
+  merge({}, baseClientWebpackConfig, commonOptions)
 
 module.exports = commonWebpackConfig
