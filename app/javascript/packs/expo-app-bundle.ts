9e5ea2be6ba0018dbfc666fc9172f6ff2581a8d7
pulkitkkr <pulkitkkr@gmail.com>
2021-05-14 05:28:45 +0530

    Address Self Review

--- app/javascript/packs/expo-app-bundle.ts
+++ app/javascript/packs/expo-app-bundle.ts
@@ -2,7 +2,7 @@ import ReactOnRails from 'react-on-rails'
 
 import ExpoApp from '../bundles/expo-app/App'
 
-// This is how react_on_rails can see the HelloWorld in the browser.
+// This is how react_on_rails can see the ExpoApp in the browser.
 ReactOnRails.register({
   ExpoApp
 })
