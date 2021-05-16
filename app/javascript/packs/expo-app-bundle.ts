2aa44aa8ea5262fd5f5eafcc9d3e3c7a4ab6a7cb
pulkitkkr <pulkitkkr@gmail.com>
2021-05-16 17:49:13 +0530

    Sync Packages

--- app/javascript/packs/expo-app-bundle.ts
+++ app/javascript/packs/expo-app-bundle.ts
@@ -1,7 +1,7 @@
 import ReactOnRails from 'react-on-rails'
 
 import ExpoAppWithoutNavigation from '../bundles/expo-without-navigation/App'
-import ExpoAppWithNavigation from '../bundles/expo-with-navigation/App'
+import ExpoAppWithNavigation from '../bundles/expo-with-react-navigation/App'
 
 // This is how react_on_rails can see the Expo Apps in the browser.
 ReactOnRails.register({
