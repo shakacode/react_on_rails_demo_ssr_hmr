e6ca51f5cb3941a71646cdcf3763e8b9c941ac90
pulkitkkr <pulkitkkr@gmail.com>
2021-06-03 16:51:04 +0530

    CH-42 - Refactor Config & Get Hot Reloading Working for Expo Android and iOS

--- app/javascript/bundles/expo-with-react-navigation/metro.config.js
+++ app/javascript/bundles/expo-with-react-navigation/metro.config.js
@@ -1,22 +1,36 @@
+const { getDefaultConfig } = require('expo/metro-config')
 const path = require('path')
 
-const extraNodeModules = {
-  react: path.resolve(__dirname + '/../../../../node_modules/react'),
-  'react-dom': path.resolve(__dirname + '/../../../../node_modules/react-dom')
-}
-const watchFolders = [path.resolve(__dirname + '/../../../../node_modules')]
+// This is the default configuration for Expo Project
+const config = getDefaultConfig(__dirname)
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false
-      }
-    })
-  },
-  resolver: {
-    extraNodeModules
-  },
-  watchFolders
+// This is the relative path to root node_modules
+const projectRootNodeModules = '../../../../node_modules'
+
+const getPathForRootNodeModule = moduleName => {
+  return path.resolve(__dirname + `/${projectRootNodeModules}/${moduleName}`)
 }
+
+// modules to use in Android & iOS App from root directory
+const extraNodeModules = {
+  react: getPathForRootNodeModule('react'),
+  'react-dom': getPathForRootNodeModule('react-dom')
+}
+
+// Reload App if any of the root node_modules changes
+const additionalWatchFolders = [
+  path.resolve(__dirname + projectRootNodeModules)
+]
+
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: false
+  }
+})
+
+config.resolver.extraNodeModules = extraNodeModules
+
+config.watchFolders = [...config.watchFolders, ...additionalWatchFolders]
+
+module.exports = config
