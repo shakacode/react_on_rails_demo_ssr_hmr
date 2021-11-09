a37f0665778b5b8a47bea2395ecb6593b6c6f814
Justin Gordon <justin@shakacode.com>
2021-11-08 14:51:27 -1000

    Update the webpack config
    only for dev server
    only when inlining css

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,9 @@ module.exports = function (api) {
   const defaultConfigFunc = require('@rails/webpacker/package/babel/preset.js')
   const resultConfig = defaultConfigFunc(api);
 
+  // Example of how to modify the default
   const changesOnDefault = {
-    plugins: [
-      process.env.WEBPACK_SERVE && 'react-refresh/babel'
-    ].filter(Boolean),
+    plugins: []
   }
 
   resultConfig.plugins = [...resultConfig.plugins, ...changesOnDefault.plugins ];
--- config/webpack/development.js
+++ config/webpack/development.js
@@ -8,22 +8,6 @@ const webpackConfig = require('./webpackConfig')
 
 const developmentEnvOnly = (clientWebpackConfig, serverWebpackConfig) => {
 
-  const isWebpackDevServer = process.env.WEBPACK_DEV_SERVER
-
-  //plugins
-  if (inliningCss ) {
-    // Note, when this is run, we're building the server and client bundles in separate processes.
-    // Thus, this plugin is not applied.
-    const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
-    clientWebpackConfig.plugins.push(
-      new ReactRefreshWebpackPlugin({
-        overlay:{
-          sockPort: devServer.port
-        }
-      })
-    )
-  }
-
   clientWebpackConfig.plugins.push(
     new ForkTsCheckerWebpackPlugin({
       typescript: {
