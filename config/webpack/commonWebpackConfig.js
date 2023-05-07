6271abfdf749dcd81fbef7791e448cbad1c23f01
Justin Gordon <justin@shakacode.com>
2023-05-06 16:36:29 -1000

    WORKS

--- config/webpack/commonWebpackConfig.js
+++ config/webpack/commonWebpackConfig.js
@@ -21,8 +21,11 @@ const commonOptions = {
   }
 }
 
+const ignoreWarningsConfig = {
+  ignoreWarnings: [/Module not found: Error: Can't resolve 'react-dom\/client'/],
+};
 // Copy the object using merge b/c the baseClientWebpackConfig and commonOptions are mutable globals
 // const commonWebpackConfig = () => (merge({}, baseClientWebpackConfig, commonOptions))
-const commonWebpackConfig = () => (merge({}, baseClientWebpackConfig(), commonOptions))
+const commonWebpackConfig = () => (merge({}, baseClientWebpackConfig(), commonOptions, ignoreWarningsConfig))
 
 module.exports = commonWebpackConfig
